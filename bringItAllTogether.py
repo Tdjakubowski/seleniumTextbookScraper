95a39737a3e6ec2848271e6aa8781ab364e4d8fb
Tdjakubowski <91572170+Tdjakubowski@users.noreply.github.com>
2021-11-30 19:33:35 -0600

    Update bringItAllTogether.py
    Bug fixes, prints all textbooks in a legible way, and prints the cheapest book last

--- bringItAllTogether.py
+++ bringItAllTogether.py
@@ -6,21 +6,21 @@ def main():
     googleList = []
     results = []
     individualResults = []
-    className = input('Input Class Name:')
+    className = input('Input Class Name:').upper()
     section = 'Section:'+input('Input 3 Digit Section Number(ex: 001):')
-    path = 'textbookList.txt'
-    textbook = sL.searchTextbookList(className,section,path)
+    browser = input('Input 1 for Firefox(GeckoDriver), or 2 for Chrome(ChromeDriver):')
+    textbook = sL.searchTextbookList(className,section,'textbookList.txt')
 
     #If it doesn't return a textbook, adds a space inbetween className
     for i in range(2):
         if textbook == None:
-            textbook = sL.searchTextbookList(className[:3]+' '+className[3:],section,path)
+            textbook = sL.searchTextbookList(className[:3]+' '+className[3:],section,'textbookList.txt')
 
     if str(textbook[2]) == 'No Textbook Assigned':
         print('No Textbook Assigned')
         return 'No Textbook Assigned'
     else:
-        driver = scraper.seleniumStartup(1)    
+        driver = scraper.seleniumStartup(browser)    
     for x in range(2,len(textbook)): #Will run several times if multiple ISBN's
         resultsRetrieved = False
         while not resultsRetrieved and i<5:
@@ -37,7 +37,7 @@ def main():
         
         try:
             for book in googleList:
-                results.append(book)
+                individualResults.append(book)
         except AttributeError:
             big = 'sad' #Google couldn't get any textbooks
         if isuList != None:
@@ -47,9 +47,22 @@ def main():
         results.append(individualResults)
             
     driver.quit()
-    print(results)
-    return results
-        
+
+    for i in range(len(results)):
+        cheapest = 99999.99
+        cheapestIndex = 999
+        print('\n')
+        print(r'////////////////////Book '+str(i+1)+r'\\\\\\\\\\\\\\\\\\\\')
+        print('\n')
+        for x in range(len(results[i])):
+            print('Price: '+str(results[i][x][0])+' Seller: '+str(results[i][x][1])+' URL: '+str(results[i][x][2])+'\n')
+            try:
+                if cheapest > float(results[i][x][0][1:]):
+                    cheapestIndex = x
+            except ValueError:
+                if cheapest > float(results[i][x][0][2:]):
+                    cheapestIndex = x
+        print('\n Cheapest Book:\nPrice: '+str(results[i][cheapestIndex][0])+' Seller: '+str(results[i][cheapestIndex][1])+' URL: '+str(results[i][cheapestIndex][2])+'\n')
     
 
 main()
